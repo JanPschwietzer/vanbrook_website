d284cc4cad9216d4db90bfeecb17ebff56827a14
Jan Schwietzer <schwietzer.jan@gmail.com>
2021-05-16 19:08:12 +0200

    changed Javascript from changing innerHTML to style, visibility.

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let counter = 0;
 function menu() {
     if (counter == 0)
     {
-        document.getElementById("openmenu").innerHTML = '<a href="https://soundcloud.com/philvanbrook" target="_blank">Soundcloud</a><a href="https://open.spotify.com/artist/64kc5LSOxVU7RE0hNua1Ot?si=2WOdlfBiR82v1PMLSothoA" target="_blank">Spotify</a><a href="https://www.youtube.com/philvanbrook" target="_blank">YouTube</a><a href="https://www.twitch.tv/philvanbrook" target="_blank">Twitch</a>';
+        document.getElementById("openmenu").style.visibility = "visible";
         counter = 1;
     } else {
-        document.getElementById("openmenu").innerHTML = "";
+        document.getElementById("openmenu").style.visibility = "hidden";
         counter = 0;
     }
 }
